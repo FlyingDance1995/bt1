5ce876a8c8e307d274c7348257f309c517bc4925
FlyingDance1995 <39576535+FlyingDance1995@users.noreply.github.com>
2018-05-29 00:09:26 +0700

    bt5

---
+++ src/bt5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	int ngay, thang, nam;
+
+	printf("Ngay: ");
+	scanf_s("%d", &ngay);
+
+	printf("Thang: ");
+	scanf_s("%d", &thang);
+
+	printf("Nam: ");
+	scanf_s("%d", &nam);
+
+	if (ngay <0 || ngay >31 || thang <0 || thang >12)
+	{
+		printf("Khong hop le");
+	}
+	else
+	{
+		if (((nam % 4 == 0) && (nam % 100 != 0)) || (nam % 400 == 0))
+			printf("Nam Nhuan");
+		else
+			printf("Khong phai la Nam Nhuan");
+	}
+
+
+	_getch();
+	return 0;
+}
